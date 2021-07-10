96e1120888b904038c45e80b9ff0b6cb2c040764
AlvaroCtm <86389654+AlvaroCtm@users.noreply.github.com>
2021-07-09 19:41:39 -0500

    Add files via upload

---
+++ Main3.java
@@ -0,0 +1,19 @@
+package diagramas.de.fujo;
+
+import java.util.Scanner;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		// imprimir tabla del 7 hasta el 12//
+		 Scanner tb = new Scanner(System.in);
+	        int T= 7;
+	        System.out.print("tabla del 7");
+	       
+	        for(int i = 1; i<=12; i++){
+	             System.out.println(T + " * " + T + " = " + T*i);    
+	        
+	}
+
+}
+}
