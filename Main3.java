8d46fc31dc68d2fd8f375965a7cba80dd4f6c8a1
AlvaroCtm <86389654+AlvaroCtm@users.noreply.github.com>
2021-07-09 19:44:27 -0500

    Update Main3.java

--- Main3.java
+++ Main3.java
@@ -6,12 +6,13 @@ public class Main3 {
 
 	public static void main(String[] args) {
 		// imprimir tabla del 7 hasta el 12//
-		 Scanner tb = new Scanner(System.in);
-	        int T= 7;
+		Scanner tb = new Scanner(System.in);
+	        int n= 7;
 	        System.out.print("tabla del 7");
+	        System.out.print("...");
 	       
 	        for(int i = 1; i<=12; i++){
-	             System.out.println(T + " * " + T + " = " + T*i);    
+	             System.out.println(n + " * " + i + " = " + n*i);    
 	        
 	}
 
